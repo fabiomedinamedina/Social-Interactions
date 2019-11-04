5517afeac0a6f74769723c5c21a536613920e9e0
Jegox <jego031@gmail.com>
2019-11-04 08:59:11 -0500

    configuracion de puerto

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 'use strict'
 const mongo = require('./src/config/mongo.db')
 const server = require('./src/config/express')
-
+const port = 3100
 
 mongo.then(
   () => {
     console.log('Running Mongo Database')
-    server.listen(3100, () => console.log('Running Server'))
+    server.listen(port, () => console.log(`Running Server on port: ${port}`))
   },
   error => console.warn(error)
 ).catch(err => console.warn(err));
